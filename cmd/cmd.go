660516c01ae79ae9c42cb85bc4edfc565596ba04
Siddhant Vij <siddhantvij201@gmail.com>
2024-03-29 05:24:15 +0530

    feat: Support for the "up/down" arrow
    - To cycle through previous commands
    - Dependency on chzyer/readline v1.5

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,10 +1,11 @@
 package cmd
 
 import (
-	"bufio"
 	"fmt"
 	"os"
 	"strings"
+
+	"github.com/chzyer/readline"
 
 	"github.com/siddhant-vij/Pokedex/config"
 )
@@ -35,57 +36,72 @@ type PokemonType struct {
 
 func Run() {
 	cfg := &config.Config{}
-	scanner := bufio.NewScanner(os.Stdin)
+
+	rl, err := readline.New("pokedex > ")
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+	defer rl.Close()
 
 	for {
-		fmt.Print("pokedex > ")
-		if scanner.Scan() {
-			line := strings.ToLower(strings.TrimSpace(scanner.Text()))
-			args := strings.Split(line, " ")
-			switch args[0] {
-			case "map":
-				DisplayLocationAreas(cfg, true)
-			case "mapb":
-				DisplayLocationAreas(cfg, false)
-			case "explore":
-				if len(args) < 2 {
-					fmt.Println("Usage: explore <location>")
-					fmt.Println()
-					continue
-				}
-				ExplorePokemons(args[1])
-			case "catch":
-				if len(args) < 2 {
-					fmt.Println("Usage: catch <pokemon>")
-					fmt.Println()
-					continue
-				}
-				CatchPokemon(args[1])
-			case "inspect":
-				if len(args) < 2 {
-					fmt.Println("Usage: inspect <pokemon>")
-					fmt.Println()
-					continue
-				}
-				InspectPokemon(args[1])
-			case "pokedex":
-				fmt.Println("Your Pokedex:")
-				if len(pokedex) == 0 {
-					fmt.Println("  <empty>")
-				}
-				for name := range pokedex {
-					fmt.Println("  -", name)
-				}
+		line, err := rl.Readline()
+		if err != nil {
+			break
+		}
+
+		line = strings.ToLower(strings.TrimSpace(line))
+		if line == "" {
+			continue
+		}
+
+		rl.SaveHistory(line)
+
+		args := strings.Split(line, " ")
+		switch args[0] {
+		case "map":
+			DisplayLocationAreas(cfg, true)
+		case "mapb":
+			DisplayLocationAreas(cfg, false)
+		case "explore":
+			if len(args) < 2 {
+				fmt.Println("Usage: explore <location>")
 				fmt.Println()
-			case "exit":
-				os.Exit(0)
-			case "help":
-				PrintHelp()
-			case "clear":
-				fmt.Print("\033[2J\033[1;1H")
-			default:
-				fmt.Printf("Unknown command: %s\n", line)
+				continue
 			}
+			ExplorePokemons(args[1])
+		case "catch":
+			if len(args) < 2 {
+				fmt.Println("Usage: catch <pokemon>")
+				fmt.Println()
+				continue
+			}
+			CatchPokemon(args[1])
+		case "inspect":
+			if len(args) < 2 {
+				fmt.Println("Usage: inspect <pokemon>")
+				fmt.Println()
+				continue
+			}
+			InspectPokemon(args[1])
+		case "pokedex":
+			fmt.Println("Your Pokedex:")
+			if len(pokedex) == 0 {
+				fmt.Println("  <empty>")
+			}
+			for name := range pokedex {
+				fmt.Println("  -", name)
+			}
+			fmt.Println()
+		case "exit":
+			os.Exit(0)
+		case "help":
+			PrintHelp()
+			fmt.Println()
+		case "clear":
+			fmt.Print("\033[2J\033[1;1H")
+		default:
+			fmt.Printf("Unknown command: %s\n\n", line)
 		}
 	}
 }
